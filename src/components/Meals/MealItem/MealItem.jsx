051b5cb5f0c306ec0206eb038aa438a6fa7d34fa
CepotOi <rizkihutama69@gmail.com>
2022-09-11 15:19:25 +0700

    adding individual meal item & displaying them

---
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -0,0 +1,19 @@
+import classes from './MealItem.module.css';
+
+const MealItem = props => {
+  const mealPrice = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{mealPrice}</div>
+      </div>
+
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItem;
